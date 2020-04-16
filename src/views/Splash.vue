082136136b9834d02649e19957af43ccc3100b37
YujinHyeon <gusdbwls9772@gmail.com>
2020-04-16 17:58:19 +0900

    Add copyright

--- src/views/Splash.vue
+++ src/views/Splash.vue
@@ -10,6 +10,9 @@
           <v-flex class="pa-0" xs12 sm12 md12 lg12 xl2>
               <img src="../assets/loading.gif">
           </v-flex>
+          <v-flex class="pa-0" xs12 sm12 md12 lg12 xl2>
+              <p>COPYRIGHT@SPRINGCLOUD INC. ALL RIGHTS RESERVED.</p>
+          </v-flex>
       </v-layout>
   </v-container>
 </template>
