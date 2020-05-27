375947110c4fa6b0dcd35cc1c841d13e9863d627
HyunYuJin <gusdbwls9772@gmail.com>
2020-05-27 14:53:09 +0900

    change url

--- src/components/Static/Userinfo.vue
+++ src/components/Static/Userinfo.vue
@@ -3,21 +3,84 @@
     <v-container class="pa-0 ma-0 flex-wrap" fluid justify-center grid-list-md fill-height>
         <v-layout row wrap class="ma-0">
             <v-flex class="pa-0" xs12 sm12 md12 style="width: 100%; height: 100%;">
-                <v-list>
-                    <v-list-item>
-                        <v-list-item-content>
-                            <v-list-item-title>이름</v-list-item-title>
-                        </v-list-item-content>
+                <v-card class="pa-2" flat tile>
+                    <v-row no-gutters>
+                        <v-col cols="2">
+                            이름
+                        </v-col>
+                        <v-col cols="8">
+                            현유진
+                        </v-col>
+                        <v-col cols="2">
+                            <v-img src="../../assets/modify-icon.svg"></v-img>
+                        </v-col>
+                    </v-row>
+                </v-card>
+                <!-- <v-card flat tile>
+                    <v-list xs12 sm12 md12>
+                        <v-list-item-group>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title>이름</v-list-item-title>
+                                </v-list-item-content>
+                                <v-list-item-content>
+                                    <v-list-item-title>현유진</v-list-item-title>
+                                </v-list-item-content>
+                                <v-list-item-action>
+                                    <v-img src="../../assets/modify-icon.svg"></v-img>
+                                </v-list-item-action>
+                            </v-list-item>
 
-                        <v-list-item-content>
-                            <v-list-item-title>현유진</v-list-item-title>
-                        </v-list-item-content>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title>이메일</v-list-item-title>
+                                </v-list-item-content>
+                                <v-list-item-content>
+                                    <v-list-item-title>yjhyeon@aspringcloud.com</v-list-item-title>
+                                </v-list-item-content>
+                                <v-list-item-action>
+                                    <v-img src="../../assets/modify-icon.svg"></v-img>
+                                </v-list-item-action>
+                            </v-list-item>
 
-                        <v-list-item-action>
-                            <v-img src="'../../assets/modify-icon.svg"></v-img>
-                        </v-list-item-action>
-                    </v-list-item>
-                </v-list>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title>성별</v-list-item-title>
+                                </v-list-item-content>
+                                <v-list-item-content>
+                                    <v-list-item-title>여자</v-list-item-title>
+                                </v-list-item-content>
+                                <v-list-item-action>
+                                    <v-img src="../../assets/modify-icon.svg"></v-img>
+                                </v-list-item-action>
+                            </v-list-item>
+
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title>생일</v-list-item-title>
+                                </v-list-item-content>
+                                <v-list-item-content>
+                                    <v-list-item-title>970702</v-list-item-title>
+                                </v-list-item-content>
+                                <v-list-item-action>
+                                    <v-img src="../../assets/modify-icon.svg"></v-img>
+                                </v-list-item-action>
+                            </v-list-item>
+
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title>전화번호</v-list-item-title>
+                                </v-list-item-content>
+                                <v-list-item-content>
+                                    <v-list-item-title>010-1234-5678</v-list-item-title>
+                                </v-list-item-content>
+                                <v-list-item-action>
+                                    <v-img src="../../assets/modify-icon.svg"></v-img>
+                                </v-list-item-action>
+                            </v-list-item>
+                        </v-list-item-group>
+                    </v-list> 
+                </v-card>-->
                 <v-divider></v-divider>
             </v-flex>
         </v-layout>
