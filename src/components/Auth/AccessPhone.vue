f651153e3bc8fcabc6ef72e77415006d84b4afa4
HyunYuJin <gusdbwls9772@gmail.com>
2020-06-16 15:56:06 +0900

    Disabled

--- src/components/Auth/AccessPhone.vue
+++ src/components/Auth/AccessPhone.vue
@@ -1,6 +1,6 @@
 <template>
 <v-app style="position: relative;">
-    <v-container fluid v-if="loading == true" style="display: flex; position: absolute; background: rgba(0, 0, 0, 0.5); height: 100%; pointer-events: none; z-index: 20;">
+    <v-container fluid v-if="loading == true" style="display: flex; position: absolute; background: rgba(0, 0, 0, 0.5); height: 100%; pointer-events: none !important; z-index: 20;">
         <v-row align="center" justify="center">
             <v-card color="transparent" flat>
                 <v-card-text class="text-center">
@@ -21,7 +21,8 @@
                     <v-card-text class="pa-0 auth-phone-content">SMS로 인증번호를 보냅니다.</v-card-text>
                     <v-card-actions class="pa-0 pt-9">
                         <!-- +82 -->
-                        <input class="PhoneField d-flex pa-2 pl-4" type="number" v-model="phNo" placeholder="'-' 없이 번호만 입력해주세요." />
+                        <input class="PhoneField d-flex pa-2 pl-4" type="number" v-model="phNo" placeholder="'-' 없이 번호만 입력해주세요." disabled v-if="loading == true" />
+                        <input class="PhoneField d-flex pa-2 pl-4" type="number" v-model="phNo" placeholder="'-' 없이 번호만 입력해주세요." v-else />
                     </v-card-actions>
                 </v-card>
             </v-flex>
@@ -31,7 +32,8 @@
             <v-flex xs12 sm12 md12 class="d-flex align-end pb-0">
                 <v-flex xs12 sm12 md12 class="pa-0 justify-space-between">
                     <v-card class="text-left pa-0" color="transparent" flat tile>
-                        <v-btn depressed tile color="#E61773" id="sign-in-button" width="100%" height="50px" class="phone-next" @click="sendOtp">인증번호 받기</v-btn>
+                        <v-btn depressed tile color="#E61773" id="sign-in-button" width="100%" height="50px" class="phone-next" disabled v-if="loading == true">인증번호 받기</v-btn>
+                        <v-btn depressed tile color="#E61773" id="sign-in-button" width="100%" height="50px" class="phone-next" @click="sendOtp" v-else>인증번호 받기</v-btn>
                     </v-card>
                 </v-flex>
             </v-flex>
