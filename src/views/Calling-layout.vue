5d7683fe61b4112a2720911d420c2fdc1fdd885f
HyunYuJin <gusdbwls9772@gmail.com>
2020-07-14 15:12:04 +0900

    redirect query station_startId & station_endId

--- src/views/Calling-layout.vue
+++ src/views/Calling-layout.vue
@@ -84,7 +84,6 @@ export default {
 
     created() {
         console.log('calling-layout uid: ', this.uid);
-        this.getStation();
         this.onOpenWebsocket();
         this.onMessageWebSocket();
         // this.socket.onerror = (error) => {
