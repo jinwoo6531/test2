8eff2bfdee94d5183c1305bdc1191c0d1510da20
dwyug <dwyug@aspringcloud.com>
2020-08-26 08:32:57 +0000

    websocket secure adapt and checked it's work

--- src/views/Calling-layout.vue
+++ src/views/Calling-layout.vue
@@ -151,9 +151,10 @@ export default {
         },
 
         onOpenWebsocket() {
-            this.socket = new WebSocket("wss://222.114.39.8:11511", ["soap","wamp"],{
-                rejectUnauthorized: false
-            });
+            // this.socket = new WebSocket("wss://ws.tasio.io:11511", [],{
+            //    rejectUnauthorized: false
+            //});
+            this.socket = new WebSocket("wss://ws.tasio.io:11511", [] );
             // this.socket = new WebSocket("ws://222.114.39.8:11411");
             this.socket.onopen = (event) => {
                 console.log('onopen', event);
