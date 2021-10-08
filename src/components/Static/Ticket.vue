25ca6300e124b5122e369c04a153b6dee6096494
“dw <jyjang@aspringcloud.com>
2021-10-08 09:35:23 +0900

    merge jinwoo

--- src/components/Static/Ticket.vue
+++ src/components/Static/Ticket.vue
@@ -1,15 +1,85 @@
 <template>
     <div id="ticket">
-        <div
-            class="active-ticket"
-            v-for="ticket in ticketList[0]"
-            :key="ticket.reservation_seq"
-            :v-if="ticket.length > 0"
-        >
-            <div
+        <v-tabs v-model="tab" centered fixed-tabs>
+                <v-tab v-for="item in menus" :key="item">{{item}}</v-tab>
+
+                 <!--1회 승차권  -->
+                <v-tab-item>
+                    <div
+                    class="active-ticket"
+                    v-for="ticket in ticketList[0]"
+                    :key="ticket.reservation_seq"
+                    :v-if="ticket.length > 0"
+                    
+                    >
+                <div
                 class="ticket-tit"
                 v-bind:class="[
                     { 'ticket-tit': ticket.state === 1 },
+                    { 'deactive-ticket-tit': ticket.state === 2 },
+                ]"
+                >
+                <p>{{ ticket.date }}</p>
+                <p>
+                    {{
+                        ticket.adult_num + ticket.child_num + ticket.baby_num
+                    }}매
+                </p>
+            </div>
+  
+            <div class="ticket-main">
+                <div class="ticket-main-txt">
+                    <div class="ticket-main-tit">
+                        <img src="../../assets/tasio-logo3.png" alt="" />
+                            <!-- <img src="../../assets/qrqr.png" alt="" class="tit-img" /> -->
+                            <qrcode-stream v-if="isShowingCamera" @init="onInit" @decode="onDecode"></qrcode-stream>
+                            <div class="ticket-main-tit-div">
+                            <span>{{ticket.station_name}} {{ticket.schedule_time}}</span> 
+                            <span class="ticket-main-tit-div-span"><img src="../../assets/arrow-right.png" alt="arrow-right"/> </span>
+                            <span>{{ticket.destination}}</span>                
+                            </div>  
+                    </div>
+                     <div class="count-info">
+                        <div class="count-info-div">
+                        일반 {{ ticket.adult_num }}명,
+                        청소년 {{ ticket.child_num }}명,
+                        유아 {{ ticket.baby_num }}명
+                        </div>
+                        
+                        <div>
+                         <v-btn @click="cancelPay()" small color="white">
+                            환불하기
+                        </v-btn>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- no ticket -->
+        <div class="no-ticket" v-if="ticketList[0].length === 0">
+            <h3>구매하신 승차권이 없어요</h3>
+            <img src="../../assets/no_ticket_02.svg" />
+            <p class="no-ticket-text" style="font-size:13px">
+                구매하신 승차권이 없습니다. <br />이전 페이지나 메인으로
+                이동해주세요.
+            </p>
+        </div>
+                </v-tab-item>
+
+                <!-- 종일 승차권 -->
+                <v-tab-item>
+                    <div
+                    class="active-ticket"
+                    v-for="ticket in allDayTicket[0]"
+                    :key="ticket.reservation_seq"
+                    :v-if="ticket.length > 0"
+                    >
+            <!-- 상단 -->
+            <div
+                class="ticket-tit2"
+                v-bind:class="[
+                    { 'ticket-tit2': ticket.state === 1 },
                     { 'deactive-ticket-tit': ticket.state === 2 },
                 ]"
             >
@@ -20,47 +90,34 @@
                     }}매
                 </p>
             </div>
+            <!-- 메인 -->
             <div class="ticket-main">
                 <div class="ticket-main-txt">
                     <div class="ticket-main-tit">
-                        <p
-                            v-bind:class="{
-                                'deactive-ticket-main-tit': ticket.state === 2,
-                            }"
-                        >
-                            <span>{{ ticket.station_name }}</span> &#9;<span>{{
-                                ticket.schedule_time
-                            }}</span>
-                        </p>
-                    </div>
-                    <div class="count-info">
-                        일반 {{ ticket.adult_num }}명<br />
-                        청소년/어린이 {{ ticket.child_num }}명<br />
-                        유아{{ ticket.baby_num }}명<br />
-                    </div>
-                    <div class="payment-date">
-                        발급일시 {{ ticket.payment_date }}
+                            <img src="../../assets/tasio-logo3.png" alt="" />
+                            <img src="../../assets/qrqr.png" alt="" class="tit-img" />       
+                            <span>여수 스페이스 M 종일 승차권</span>                        
                     </div>
                 </div>
-                <p class="stamp" @click="stampClicked(ticket.reservation_seq)">
-                    <img
-                        v-if="ticket.state === 1"
-                        src="../../assets/ticket_active_stamp.png"
-                        alt="Tasio Stamp"
-                    />
-                    <img
-                        v-else-if="ticket.state === 2"
-                        src="../../assets/deactive_stamp.png"
-                        alt="Tasio Stamp"
-                    />
-                </p>
-                <!-- <vue-qr text="Hello world!" :callback="test" qid="testid" :size=80></vue-qr> -->
-                <button class="cancel" @click="cancelPay()">
-                    환불하기
-                </button>
+                <div class="count-info">
+                    <div class="count-info-div">
+                      일반 {{ ticket.adult_num }}명,
+                      청소년 {{ ticket.child_num }}명,
+                      유아 {{ ticket.baby_num }}명
+                    </div>
+                        
+                    <div>
+                      <v-btn @click="cancelPay()" small color="white">
+                        환불하기
+                      </v-btn>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="no-ticket" v-if="ticketList[0].length === 0">
+
+
+        <!-- no ticket -->
+        <div class="no-ticket" v-if="allDayTicket[0].length === 0">
             <h3>구매하신 승차권이 없어요</h3>
             <img src="../../assets/no_ticket_02.svg" />
             <p class="no-ticket-text" style="font-size:13px">
@@ -68,6 +125,11 @@
                 이동해주세요.
             </p>
         </div>
+
+            </v-tab-item>
+        </v-tabs>
+
+
         <v-footer class="copyrightStyle">
             <span
                 >결제하신 셔틀 탑승 시 승차권을 제시해주시기 바랍니다<br />
@@ -80,10 +142,10 @@
 <script>
 import { mapGetters, mapState } from "vuex"
 import axios from "axios"
-import VueQr from "vue-qr"
+
+
 export default {
     name: "ticket",
-
     data: () => ({
         date: "",
         ticketCount: 0,
@@ -97,10 +159,14 @@ export default {
         child_num: "",
         baby_num: "",
         ticketList: [],
+        allDayTicket: [],
         isrefund: "",
         merchant_uid: "",
         amount: 0,
         showModal: false,
+        menus: ['1회 승차권', '종일 승차권'],
+        tab: null,
+        isShowingCamera: true,
     }),
 
     computed: {
@@ -111,11 +177,15 @@ export default {
     },
     created() {
         this.getTicketInfo()
+        this.getAllDayTicketInfo()
     },
+    
     methods: {
-        test(dataUrl, id) {
-            console.log(VueQr, id)
+          test(dataUrl,id){
+            console.log(dataUrl, id)
         },
+
+        //1회 승차권 불러오기
         getTicketInfo() {
             console.log("uid", this.uid)
             axios
@@ -124,12 +194,13 @@ export default {
                     {
                         params: {
                             userid: this.uid,
+                            ticket_type:0
                         },
                     }
                 )
 
                 .then((res) => {
-                    console.log(res)
+                    console.log(123,res);
                     this.ticketList.push(res.data)
                     console.log("this ticket", this.ticketList)
                     console.log("예약정보 :", res.data)
@@ -141,6 +212,35 @@ export default {
                     console.log(error)
                 })
         },
+
+        getAllDayTicketInfo() {
+            axios
+                .get(
+                    `https://tasioapi.springgo.io/api/reservations/user-reservation/`,
+                    {
+                        params: {
+                            userid: this.uid,
+                            ticket_type:1
+                        },
+                    }
+                )
+
+                .then((res) => {
+                    console.log(456,res);
+                    this.allDayTicket.push(res.data)
+                    console.log("this ticket", this.allDayTicket)
+                    this.merchant_uid = res.data[0].merchant_uid
+                    this.amount = res.data[0].amount
+                    this.data = res.data[0].date
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+        },
+
+
+
+
         //티켓 정보 받아오는 api
         stampClicked(ticketId) {
             console.log("ticketId", ticketId)
@@ -198,6 +298,7 @@ export default {
 </script>
 
 <style>
+
 .payment-date {
     font-size: x-small;
     color: gray;
@@ -216,6 +317,7 @@ export default {
     width: 100%;
     height: 100%;
     position: relative;
+    
 }
 .no-ticket-text {
     color: #828282;
@@ -234,62 +336,92 @@ export default {
 }
 .active-ticket {
     width: 340px;
-    height: 150px;
+    height: 376px;
     border-radius: 5px;
     border: 1px solid #dbdbdb;
     margin: 0 auto 20px;
+    margin-top:23px;
 }
-.ticket-tit {
+.ticket-tit2 {
+    font-size: 14px;
     display: inline-flex;
     justify-content: space-between;
-    width: 100%;
-    height: 35px;
+    width: 340px;
+    height: 42px;
+    background-color: #12ABBF;
+    border-top-right-radius: 5px;
+    border-top-left-radius: 5px;
+    color: #fff;
+    padding: 10px 20px;
+    box-sizing: border-box;
+}
+.ticket-tit {
+    font-size: 14px;
+    display: inline-flex;
+    justify-content: space-between;
+    width: 340px;
+    height: 42px;
     background-color: #e61773;
     border-top-right-radius: 5px;
     border-top-left-radius: 5px;
     color: #fff;
-    padding: 5px 20px;
+    padding: 10px 20px;
     box-sizing: border-box;
+    
+}
+.tit-img {
+    padding-bottom: 10px;
 }
 .deactive-ticket-tit {
     background-color: #999;
 }
 .ticket-main {
     display: flex;
-    justify-content: space-between;
+    flex-direction: column;
     position: relative;
     margin: 20px;
+    height: 64px;
+    width:304px;
 }
 .ticket-main-txt p {
     margin-bottom: -5px;
-    font-size: 13px;
+    font-size: 14px;
 }
-.ticket-main-tit {
+.ticket-main-tit { 
     display: flex;
-    color: #e61773;
-    font-weight: 500;
-    padding-bottom: 10px;
-    margin-top: -13px;
+    align-items: center;
+    flex-direction: column;
+    color:black;
+    padding-bottom: 20px;
+}
+.ticket-main-tit-div {
+    display: flex;
+    justify-content: space-between;
+}
+.ticket-main-tit-div-span {
+   margin-left: 5px;
+   padding-top:2px; 
+}
+.arrow-right {
+    padding-top: 10px;
 }
 .deactive-ticket-main-tit {
     color: #555;
     font-weight: 500;
 }
 .count-info {
-    font-size: 12px;
-}
-.stamp {
     display: flex;
-    flex-direction: column;
-    position: absolute;
-    top: 30%;
-    right: 0;
-    transform: translateY(-50%);
+    justify-content: space-between;
+    padding: 10px;
+    width: 304px;
+    height: 64px;
+    font-size: 12px;
+    border-radius: 3px;
+    border:1px solid #DBDBDB;
+    background-color: #F9F9F9;
 }
-.stamp span {
-    /* color: #e61773; */
-    /* font-size: 12px; */
-    /* font-weight: 500; */
+.count-info-div {
+    padding:7px;
 }
 
 .copyrightStyle {
